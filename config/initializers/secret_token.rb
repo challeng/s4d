edaf0b55fa89c3899a7ce4272312d83f37179153
Jim Challenger <jim.challenger1@gmail.com>
2012-08-07 19:46:58 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sires4dota::Application.config.secret_token = 'acfa0991b679d19ab2347b66026157a840eeb1c80c9440ff9c0f8ea8adcd892a73819edafbd8291097284a13ce92e07ef80a034b5d12fa23f77742d0f2921548'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Sires4dota::Application.config.session_store :cookie_store, key: '_sires4dota_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Sires4dota::Application.config.session_store :active_record_store
